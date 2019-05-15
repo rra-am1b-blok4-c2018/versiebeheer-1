6cef8b6605c90b49052c29b1837b7dc68ea97f67
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-05-15 15:31:57 +0200

    vijfde commit

--- class_text.php
+++ class_text.php
@@ -4,6 +4,7 @@
     var $wattekst = "Dit is een tekst met een alternative fact erin.";
     var $nieuwetekst = "Dit is de derde commit die ik maak";
     var $tekst1 = "Dit is de vierde commit die ik maak";
+    var $tekst2 = "Dit is de vijfde commit die ik maak";
 
   }
 ?>
